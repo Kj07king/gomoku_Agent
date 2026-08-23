a11da537913b8c8604691be3f6a6ee21b4192b24
Kj07king <kabirkimtani108@gmail.com>
2026-08-23 16:01:50 +0200

    Add play method to handle game moves
    code play method for game logic and move decision.

--- code_file.py
+++ code_file.py
@@ -8,3 +8,24 @@ class GomokuAgent:
     self.opponent_symbol = opponent_symbol
     self.time_limit = 4.2  # it makes the move decison in less then 5 seconds 
     self.position_matrix = None
+  
+  def play(self, board):
+        start_time = time.time()
+        rows = len(board)
+        cols = len(board[0]) if rows > 0 else 0
+
+        # Create of change matrix-based position bias in the board dimensions
+        if self.position_matrix is None or len(self.position_matrix) != rows or len(self.position_matrix[0]) != cols:
+            self.position_matrix = self._generate_position_matrix(rows, cols)
+        
+        #symbol dectection can play as X or 0
+        self.detect_symbols(board, rows, cols)
+        
+        #makes a move within 1 distance of the opponents move
+        opponents = self.opponents_move(board, rows, cols, distance=1)
+
+        #Opening move: starts with the center of the board
+        if not candidates:
+          return(rows //2, cols // 2)
+          
+       
